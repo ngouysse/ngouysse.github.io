b20eee9c59810357ebf8e9ec5f5432e18a0d1441
ngouysse <101601091+ngouysse@users.noreply.github.com>
2026-02-02 17:56:33 +0100

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-fetch("https://webhook.site/9ab94d7f-8be6-4943-92e8-8a373b5da1c3"),{
+fetch("https://webhook.site/9ab94d7f-8be6-4943-92e8-8a373b5da1c3",{
   method: "POST",
   body: "toto le fou qui envoie un message",
   mode: "no-cors"
